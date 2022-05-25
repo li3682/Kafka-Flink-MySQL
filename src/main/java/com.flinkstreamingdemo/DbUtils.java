2424b6df3f30a9bf8594ce4710ad4e2d902a3e41
Jarrett <72728690+li3682@users.noreply.github.com>
2022-05-25 11:37:21 +0800

    Update DbUtils.java

--- src/main/java/com.flinkstreamingdemo/DbUtils.java
+++ src/main/java/com.flinkstreamingdemo/DbUtils.java
@@ -10,9 +10,9 @@ public class DbUtils {
     public static Connection getConnection() throws Exception {
         dataSource = new DruidDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://10.192.15.60:4306/xp_ems_t");
-        dataSource.setUsername("XP_Admin");
-        dataSource.setPassword("XP_AdminXP_Admin");
+        dataSource.setUrl("jdbc:mysql://ip:port/database");
+        dataSource.setUsername("username");
+        dataSource.setPassword("password");
         //设置初始化连接数，最大连接数，最小闲置数
         dataSource.setInitialSize(10);
         dataSource.setMaxActive(50);
